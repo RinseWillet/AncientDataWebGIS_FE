7247ad381862ca43d6fa3b33bc17c5f39b5e3923
Rinse Willet <r.willet@gmail.com>
2024-08-07 18:06:33 +0200

    cleaning up earlier commit mess, now just one working OL component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-=======
->>>>>>> 6fdfae28366b650eda40c739864defb9e24c5fe7
 import './App.css'
 import MapComponent from './components/MapComponent/MapComponent';
 
 
 const App = () => {
   return (
-<<<<<<< HEAD
     <>
       <div>
         <p>hoi</p>        
@@ -19,19 +14,6 @@ const App = () => {
       </p>
     </>
   )
-=======
-    <div className='App'>     
-        <Navbar />
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route exact path='/home' element={<Home />} />
-          <Route exact path='/about' element={<About />} />
-          <Route exact path='/atlas' element={<Atlas />} />
-        </Routes>    
-
-    </div>
-  );
->>>>>>> 6fdfae28366b650eda40c739864defb9e24c5fe7
 }
 
 export default App
