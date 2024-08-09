d777bb94215e21d84fc89f805a88773fae19906b
RinseWillet <r.willet@gmail.com>
2024-08-09 13:02:28 +0200

    busy with plotting data

--- src/services/RoadService.jsx
+++ src/services/RoadService.jsx
@@ -1,11 +1,11 @@
 import apiClient from "../api/config";
 
 const RoadService = {        
-    findAllGeoJson() {
+    async findAllGeoJson() {
         return apiClient.get('/roads/geojson');
     },
-    findAllByIdGeoJson(data) {
-        return apiClient.get('/roads/' + data.id);
+    async findAllByIdGeoJson(data) {
+        return await apiClient.get('/roads/' + data.id);
     },    
 };
 
