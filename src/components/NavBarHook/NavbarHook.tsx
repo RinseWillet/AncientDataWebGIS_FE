bfc9e3123a8af3176c2b81c6b5d51f04f269664f
r-willet <rinse.willet@luminis.eu>
2026-05-11 11:38:46 +0200

    feat: add About, News, NoPage components and NavbarHook for navigation

---
+++ src/components/NavBarHook/NavbarHook.tsx
@@ -0,0 +1,126 @@
+import { useState } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { IoClose, IoMenu } from 'react-icons/io5';
+import { useMediaQuery } from 'react-responsive';
+import { logout } from '../../features/authentication/authSlice';
+import { selectAuthUser, selectIsAdmin } from '../../features/authentication/authSelectors';
+import { resetSites } from '../../features/site/siteSlice';
+import { resetRoads } from '../../features/road/roadSlice';
+import { resetModRefs } from '../../features/modref/modRefSlice';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import './NavbarHook.css';
+
+const NavbarHook = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const isMobile = useMediaQuery({ maxWidth: '1150px' });
+
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const user = useAppSelector(selectAuthUser);
+  const isAdmin = useAppSelector(selectIsAdmin);
+
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+  const closeMobileMenu = () => isMobile && setIsMenuOpen(false);
+
+  const handleLogout = () => {
+    dispatch(logout());
+    dispatch(resetSites());
+    dispatch(resetRoads());
+    dispatch(resetModRefs());
+    navigate('/login');
+  };
+
+  const renderNavLinks = () => {
+    const listClassName = isMobile ? 'nav__list' : 'nav__list__web';
+    const linkClassName = 'nav__link';
+    const buttonClassName = 'nav__cta';
+
+    return (
+      <ul className={listClassName}>
+        <li>
+          <NavLink to="/" className={linkClassName} onClick={closeMobileMenu}>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/news" className={linkClassName} onClick={closeMobileMenu}>
+            News
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" className={linkClassName} onClick={closeMobileMenu}>
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/datalist" className={linkClassName} onClick={closeMobileMenu}>
+            DataList
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/atlas" className={`${linkClassName} ${buttonClassName}`} onClick={closeMobileMenu}>
+            To the Map
+          </NavLink>
+        </li>
+
+        {user ? (
+          <>
+            <li>
+              <span className="nav__link welcome-message">Welcome, {user.username}</span>
+            </li>
+
+            {isAdmin && (
+              <li>
+                <NavLink to="/admin-panel" className={linkClassName} onClick={closeMobileMenu}>
+                  Admin Panel
+                </NavLink>
+              </li>
+            )}
+
+            <li>
+              <button className="nav__link logout-button" onClick={handleLogout}>
+                Logout
+              </button>
+            </li>
+          </>
+        ) : (
+          <li>
+            <NavLink to="/login" className={linkClassName} onClick={closeMobileMenu}>
+              Login
+            </NavLink>
+          </li>
+        )}
+      </ul>
+    );
+  };
+
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <NavLink to="/" className="nav__logo">
+          Navigation Bar
+        </NavLink>
+
+        {isMobile && (
+          <div className="nav__toggle" id="nav-toggle" onClick={toggleMenu}>
+            <IoMenu />
+          </div>
+        )}
+
+        {isMobile ? (
+          <div className={`nav__menu ${isMenuOpen ? 'show-menu' : ''}`} id="nav-menu">
+            {renderNavLinks()}
+            <div className="nav__close" id="nav-close" onClick={toggleMenu}>
+              <IoClose />
+            </div>
+          </div>
+        ) : (
+          renderNavLinks()
+        )}
+      </nav>
+    </header>
+  );
+};
+
+export default NavbarHook;
+
---
+++ src/pages/NoPage.tsx
@@ -0,0 +1,10 @@
+const NoPage = () => {
+  return (
+    <div className="pagebox">
+      <h1>Something went wrong!</h1>
+    </div>
+  );
+};
+
+export default NoPage;
+
